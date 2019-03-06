3885d154973e90665c9da58d48a0b5ad173a7803
Eksapsy <aeksapsy@gmail.com>
2019-03-06 08:50:00 +0200

    Changed to landpage component

---
+++ src/scenes/Landpage/index.tsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import './style.scss';
+
+export class Landpage extends Component {
+  public render() {
+    return (
+      <div className='landpage'>
+        <div className='container'>
+          <header>
+            <h1>
+              Ionian IEEE Student Branch
+            </h1>
+          </header>
+        </div>
+      </div>
+    );
+  }
+}
