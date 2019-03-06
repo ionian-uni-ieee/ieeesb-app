16123673244d5396c733d2240518693f0c6c2d35
Eksapsy <aeksapsy@gmail.com>
2019-03-06 10:18:29 +0200

    Added routes class name on Router div

--- src/scenes/SceneSelector.tsx
+++ src/scenes/SceneSelector.tsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Landpage } from './Landpage';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './style.scss';
 
 export class SceneSelector extends Component {
   render() {
     return (
       <div className='scene'>
           <Router>
-              <div>
+              <div className='routes'>
                 <Route path="/" exact={true} component={Landpage} />
               </div>
           </Router>
