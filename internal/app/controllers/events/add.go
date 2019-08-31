ff0ee1ec51bf82ffae42098f0845427ef22631e5
exapsy <aeksapsy@gmail.com>
2019-08-31 20:11:42 +0300

    Change controller parameters to a model

--- internal/app/controllers/events/add.go
+++ internal/app/controllers/events/add.go
@@ -7,36 +7,30 @@ import (
 	"github.com/ionian-uni-ieee/ieee-webapp/internal/app/models"
 )
 
-func (c *Controller) Add(name string, description string, tags []string, eType string) (string, error) {
-	if name == "" {
+// Add adds an event to the repository
+func (c *Controller) Add(newEvent models.Event) (string, error) {
+	if newEvent.Name == "" {
 		return "", errors.New("Name is empty string")
 	}
 
-	if description == "" {
+	if newEvent.Description == "" {
 		return "", errors.New("Description is empty string")
 	}
 
-	for index, tag := range tags {
+	for index, tag := range newEvent.Tags {
 		if tag == "" {
 			return "", errors.New("tags[" + strconv.Itoa(index) + "] is empty string")
 		}
 	}
 
-	if eType == "" {
+	if newEvent.Type == "" {
 		return "", errors.New("Type is empty string")
 	}
 
-	isDuplicate := c.repositories.EventsRepository.IsDuplicate(name)
+	isDuplicate := c.repositories.EventsRepository.IsDuplicate(newEvent.Name)
 
 	if isDuplicate {
 		return "", errors.New("An event already exists with the same name")
-	}
-
-	newEvent := models.Event{
-		Name:        name,
-		Description: description,
-		Tags:        tags,
-		Type:        eType,
 	}
 
 	eventHexID, err := c.repositories.EventsRepository.InsertOne(newEvent)
