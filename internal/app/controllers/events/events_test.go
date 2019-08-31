6b000738fcdd748f987c84ab9ae9dacc319ab7ba
exapsy <aeksapsy@gmail.com>
2019-08-31 23:07:25 +0300

    Add mock event objects as test helpers

--- internal/app/controllers/events/events_test.go
+++ internal/app/controllers/events/events_test.go
@@ -8,7 +8,43 @@ import (
 	"github.com/ionian-uni-ieee/ieee-webapp/internal/app/models"
 	"github.com/ionian-uni-ieee/ieee-webapp/internal/app/repositories"
 	"github.com/ionian-uni-ieee/ieee-webapp/pkg/reflections"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+var mockEvent = models.Event{
+	ID:          primitive.NewObjectID(),
+	Name:        "arduino",
+	Description: "Learn how to handle Arduino microcontrollers",
+	Tags:        []string{"arduino", "hardware"},
+	Type:        "seminar",
+	Sponsors:    []models.Sponsor{},
+	Logo:        models.MediaMeta{},
+	Media:       []models.MediaMeta{},
+}
+
+var mockEvents = []models.Event{
+	mockEvent,
+	models.Event{
+		ID:          primitive.NewObjectID(),
+		Name:        "C++ from zero to hero",
+		Description: "Programming C++ for beginners that want to become heroes",
+		Tags:        []string{"language", "programming"},
+		Type:        "workshop",
+		Sponsors:    []models.Sponsor{},
+		Logo:        models.MediaMeta{},
+		Media:       []models.MediaMeta{},
+	},
+	models.Event{
+		ID:          primitive.NewObjectID(),
+		Name:        "What is Machine Learning",
+		Description: "Learn what is machine learning and how to train your own models like a pro",
+		Tags:        []string{"statistics", "machine learning", "programming", "python"},
+		Type:        "workshop",
+		Sponsors:    []models.Sponsor{},
+		Logo:        models.MediaMeta{},
+		Media:       []models.MediaMeta{},
+	},
+}
 
 func makeController() (*testingDatabase.DatabaseSession, *events.Controller) {
 	// Setup
@@ -56,7 +92,7 @@ func resetCollection(db *testingDatabase.DatabaseSession, collectionName string)
 func setupData(db *testingDatabase.DatabaseSession, collectionName string, data ...models.Event) {
 	resetCollection(db, collectionName)
 
-	ticketFieldNames, err := reflections.GetFieldNames(&models.Ticket{})
+	ticketFieldNames, err := reflections.GetFieldNames(&models.Event{})
 	if err != nil {
 		panic(err)
 	}
