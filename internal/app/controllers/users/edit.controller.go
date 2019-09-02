f6d95cb31f2678a5ef526825e2c3541b7a868895
exapsy <aeksapsy@gmail.com>
2019-09-02 21:46:02 +0300

    Implement edit controller and test

--- internal/app/controllers/users/edit.controller.go
+++ internal/app/controllers/users/edit.controller.go
@@ -2,15 +2,28 @@ package users
 
 import (
 	"errors"
+	"reflect"
 
 	"github.com/ionian-uni-ieee/ieee-webapp/internal/app/models"
+	"github.com/ionian-uni-ieee/ieee-webapp/pkg/reflections"
 )
 
 // Edit updates a user to the model provided
-func (c *Controller) Edit(userID string, edit models.User) error {
+func (c *Controller) Edit(userID string, update map[string]interface{}) error {
 	if userID == "" {
 		return errors.New("UserID is empty string")
 	}
 
-	return nil
+	isUpdateMapValid := reflections.AreMapFieldsValid(
+		reflect.TypeOf(models.User{}),
+		update,
+	)
+
+	if !isUpdateMapValid {
+		return errors.New("Update's field(s) are not valid for User model")
+	}
+
+	err := c.repositories.UsersRepository.UpdateByID(userID, update)
+
+	return err
 }
--- internal/app/controllers/users/edit.controller_test.go
+++ internal/app/controllers/users/edit.controller_test.go
@@ -1,7 +1,38 @@
 package users_test
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/ionian-uni-ieee/ieee-webapp/internal/app/models"
+	"github.com/ionian-uni-ieee/ieee-webapp/internal/app/testUtils"
+)
 
 func TestEdit(t *testing.T) {
+	// Setup
+	db, usersController := makeController()
 
+	// Ok
+	testUtils.SetupData(db, "users", mockUser)
+
+	newFullname := "New Fullname"
+	updateMap := map[string]interface{}{
+		"Fullname": "New Fullname",
+	}
+	err := usersController.Edit(mockUser.ID.Hex(), updateMap)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	storedUser := testUtils.GetInterfaceAtCollectionRow(
+		db,
+		"users",
+		reflect.TypeOf(models.User{}),
+		0,
+	).(models.User)
+
+	if storedUser.Fullname != newFullname {
+		t.Error("Expected fullname to have been changed, but it's " + storedUser.Fullname)
+	}
 }
