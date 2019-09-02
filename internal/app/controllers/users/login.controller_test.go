b485ed2ee6b41f008c45cb613acdd6baf545a964
exapsy <aeksapsy@gmail.com>
2019-09-02 21:15:09 +0300

    Add login controller error tests

--- internal/app/controllers/users/login.controller_test.go
+++ internal/app/controllers/users/login.controller_test.go
@@ -22,4 +22,32 @@ func TestLogin(t *testing.T) {
 	if sessionID == "" {
 		t.Error("Expected sessionID to be a non empty string")
 	}
+
+	// Wrong pass
+	testUtils.SetupData(db, "users", mockUser)
+
+	sessionID, err = usersController.Login(mockUser.Username, "wrong pass")
+
+	expectedError := "Password not verified"
+	if err.Error() != expectedError {
+		t.Error("Expected '" + expectedError + "' error but instead got " + err.Error())
+	}
+
+	if sessionID != "" {
+		t.Error("Expected sessionID to be empty string but instead got " + sessionID)
+	}
+
+	// User doesn't exist
+	testUtils.ResetCollection(db, "users")
+
+	sessionID, err = usersController.Login("randomuser", "randompass")
+
+	expectedError = "No user found"
+	if err.Error() != expectedError {
+		t.Error("Expected '" + expectedError + "' error but instead got " + err.Error())
+	}
+
+	if sessionID != "" {
+		t.Error("Expected sessionID to be empty string but instead got " + sessionID)
+	}
 }
