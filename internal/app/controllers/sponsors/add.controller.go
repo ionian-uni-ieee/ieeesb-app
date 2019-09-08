9ba6e4af55b610f16bea22c0d69601ce084ba6b1
exapsy <aeksapsy@gmail.com>
2019-09-09 01:31:41 +0300

    Add add & delete sponsor controllers

--- internal/app/controllers/sponsors/add.controller.go
+++ internal/app/controllers/sponsors/add.controller.go
@@ -1,8 +1,18 @@
 package sponsors
 
-import "github.com/ionian-uni-ieee/ieeesb-app/internal/app/models"
+import (
+	"errors"
+
+	"github.com/ionian-uni-ieee/ieeesb-app/internal/app/models"
+)
 
 // Add inserts a sponsor in the repository
 func (c *Controller) Add(sponsor models.Sponsor) (string, error) {
-	return "", nil
+	if sponsor.Name == "" {
+		return "", errors.New("Name is empty string")
+	}
+
+	insertedID, err := c.repositories.SponsorsRepository.InsertOne(sponsor)
+
+	return insertedID, err
 }
--- internal/app/controllers/sponsors/delete.controller.go
+++ internal/app/controllers/sponsors/delete.controller.go
@@ -1,5 +1,21 @@
 package sponsors
 
+import (
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 func (c *Controller) Delete(sponsorID string) error {
-	return nil
+	if sponsorID == "" {
+		return errors.New("SponsorID is empty string")
+	}
+
+	if _, err := primitive.ObjectIDFromHex(sponsorID); err != nil {
+		return errors.New("SponsorID is invalid ObjectID")
+	}
+
+	err := c.repositories.SponsorsRepository.DeleteByID(sponsorID)
+
+	return err
 }
