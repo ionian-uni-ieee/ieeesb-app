19bcb72e32445f4cfa75c9baf3feec717d9f905d
exapsy <aeksapsy@gmail.com>
2019-09-08 10:54:50 +0300

    Add sponsor.Add test

--- internal/app/controllers/sponsors/add.controller.go
+++ internal/app/controllers/sponsors/add.controller.go
@@ -1,1 +1,8 @@
 package sponsors
+
+import "github.com/ionian-uni-ieee/ieee-webapp/internal/app/models"
+
+// Add inserts a sponsor in the repository
+func (c *Controller) Add(sponsor models.Sponsor) (string, error) {
+	return "", nil
+}
---
+++ internal/app/controllers/sponsors/add.controller_test.go
@@ -0,0 +1,40 @@
+package sponsors_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/ionian-uni-ieee/ieee-webapp/internal/app/models"
+	"github.com/ionian-uni-ieee/ieee-webapp/internal/app/testUtils"
+)
+
+func TestAdd(t *testing.T) {
+	//Setup
+	db, sponsorsController := makeController()
+
+	t.Run("Should add a new sponsor", func(t *testing.T) {
+		testUtils.ResetCollection(db, "sponsors")
+
+		sponsorID, err := sponsorsController.Add(mockSponsor)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		firstSponsor := testUtils.GetInterfaceAtCollectionRow(
+			db,
+			"sponsors",
+			reflect.TypeOf(models.Sponsor{}),
+			0,
+		).(models.Sponsor)
+
+		if firstSponsor.ID.IsZero() {
+			t.Error("Expected sponsor id to return a non zero ID")
+			t.SkipNow()
+		}
+
+		if firstSponsor.ID.Hex() != sponsorID {
+			t.Error("Expected stored sponsor's ID to be '" + sponsorID + "' but got '" + firstSponsor.ID.Hex() + "'")
+		}
+	})
+}
