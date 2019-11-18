5bba4f0283ba4c45be9ddc536d8d45207e8c41fe
Rigas-A <p18riga@ionio.gr>
2019-11-18 04:08:38 +0200

    Add permissions model helper functions

--- internal/app/models/permissions.go
+++ internal/app/models/permissions.go
@@ -18,3 +18,43 @@ func NewPermissions(users bool, events bool, tickets bool, sponsors bool) *Permi
 		Sponsors: sponsors,
 	}
 }
+
+// GetUsers returns moderator's permissions for users
+func (p *Permissions) GetUsers() bool {
+	return p.Users
+}
+
+// SetUsers changes moderator's permissions for users
+func (p *Permissions) SetUsers(users bool) {
+	p.Users = users
+}
+
+// GetEvents returns moderator's permissions for events
+func (p *Permissions) GetEvents() bool {
+	return p.Events
+}
+
+// SetEvents changes moderator's permissions for events
+func (p *Permissions) SetEvents(events bool) {
+	p.Events = events
+}
+
+// GetTickets returns moderator's permission for tickets
+func (p *Permissions) GetTickets() bool {
+	return p.Tickets
+}
+
+// SetTickets changes moderator's permissions for tickets
+func (p *Permissions) SetTickets(tickets bool) {
+	p.Tickets = tickets
+}
+
+// GetSponsors returns moderator's permissions for sponsors
+func (p *Permissions) GetSponsors() bool {
+	return p.Sponsors
+}
+
+// SetSponsors changes moderator's permissions for sponsors
+func (p *Permissions) SetSponsors(sponsors bool) {
+	p.Sponsors = sponsors
+}
