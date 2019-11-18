e51f0ed3f2db68bb13f14ba92c06bec5777bcd00
Rigas-A <p18riga@ionio.gr>
2019-11-18 03:26:40 +0200

    Delete controllers

--- internal/app/controllers/tickets/getTickets.controller.go
+++
@@ -1,9 +0,0 @@
-package tickets
-
-import "github.com/ionian-uni-ieee/ieeesb-app/internal/app/models"
-
-func (c *Controller) GetTickets(skip int64, limit int64) ([]models.Ticket, error) {
-	tickets, err := c.repositories.TicketsRepository.Find(nil, skip, limit)
-
-	return tickets, err
-}
--- internal/app/controllers/users/getUsers.controller.go
+++
@@ -1,9 +0,0 @@
-package users
-
-import "github.com/ionian-uni-ieee/ieeesb-app/internal/app/models"
-
-func (c *Controller) GetUsers(skip int64, limit int64) ([]models.User, error) {
-	users, err := c.repositories.UsersRepository.Find(nil, skip, limit)
-
-	return users, err
-}
