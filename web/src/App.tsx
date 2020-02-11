b410af90eb224074a22bd0072e5184f3b103d5ab
Giannhsker <giannishuin@gmail.com>
2020-02-11 18:36:26 +0200

    2 files at src

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Main} from './pages'
 
 const App: React.FC = () => {
   return (
+
     <div className='App'>
       <header className='App-header'>Ionio IEEE App</header>
+      <Main/>
     </div>
   )
 }
---
+++ web/src/pages/Main/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Main = () => {
+    return (<h1>hello</h1>)
+}
+
+
+export default Main
