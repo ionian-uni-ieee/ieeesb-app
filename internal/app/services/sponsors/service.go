c19a1ebd811bb2d2b4a8a6ee51e93d57610a379b
exapsy <apostolis.anastasiou.alpha@gmail.com>
2020-02-13 14:32:28 +0200

    Add services files

---
+++ internal/app/services/sponsors/service.go
@@ -0,0 +1,15 @@
+package sponsors
+
+import "github.com/ionian-uni-ieee/ieeesb-app/internal/app/repositories"
+
+// Service is a business rule controlled entity that uses repositories to perform actions on model entities
+type Service struct {
+	repositories *repositories.Repositories
+}
+
+// MakeService is a Sponsors Service factory
+func MakeService(repositories *repositories.Repositories) *Service {
+	return &Service{
+		repositories: repositories,
+	}
+}
