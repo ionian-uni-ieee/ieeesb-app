0f2344b4b2cebdb61484017ec512d611f1679af1
exapsy <apostolis.anastasiou.alpha@gmail.com>
2020-02-16 17:42:03 +0200

    Add user service functions?

--- internal/app/services/users/add.go
+++ internal/app/services/users/add.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ionian-uni-ieee/ieeesb-app/pkg/validation"
 )
 
+// Add adds a new user to the user repository
 func (s *Service) Add(user models.User) (string, validation.Validation) {
 	v := s.Validate(user)
 	userID, err := s.repositories.Users.InsertOne(user)
