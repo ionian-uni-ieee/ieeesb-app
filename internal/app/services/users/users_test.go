99cb9a590e61cb15f6373a13f6406b42d5b83081
exapsy <apostolis.anastasiou.alpha@gmail.com>
2020-03-09 01:03:01 +0200

    Add user tests

--- internal/app/services/users/users_test.go
+++ internal/app/services/users/users_test.go
@@ -30,7 +30,7 @@ func TestValidate(t *testing.T) {
 		}
 	})
 	t.Run("Should return false for wrong email form", func(t *testing.T) {
-		validUser := models.User{
+		invalidUser := models.User{
 			ID:          primitive.NewObjectID(),
 			Email:       "wrongemailform",
 			Fullname:    "test",
@@ -39,13 +39,44 @@ func TestValidate(t *testing.T) {
 			Username:    "test",
 		}
 
-		validation := *service.Validate(validUser)
+		validation := *service.Validate(invalidUser)
 		userIsValid := !validation.HasError()
 		if userIsValid {
 			t.Error("User should be invalid")
 		}
 		if validation["email"] != users.ErrInvalidEmail {
 			t.Error("Expected \"" + users.ErrInvalidEmail.Error() + "\" but got \"" + validation["email"].Error() + "\"")
+		}
+	})
+	t.Run("Should return false for duplicate fullname, username & email", func(t *testing.T) {
+		validUser := models.User{
+			ID:          primitive.NewObjectID(),
+			Email:       "test@test.com",
+			Fullname:    "test",
+			Password:    "test",
+			Permissions: models.Permissions{},
+			Username:    "test",
+		}
+		_, err := service.Add(validUser)
+		if err != nil {
+			t.Error("Got error when adding user", err)
+		}
+		invalidUser := models.User{
+			ID:          primitive.NewObjectID(),
+			Email:       "test@test.com",
+			Fullname:    "test",
+			Password:    "test",
+			Permissions: models.Permissions{},
+			Username:    "test",
+		}
+
+		validation := *service.Validate(invalidUser)
+		userIsValid := !validation.HasError()
+		if userIsValid {
+			t.Error("User should be invalid")
+		}
+		if validation["user/email/username"] != users.ErrUserExists {
+			t.Error("Expected \"" + users.ErrUserExists.Error() + "\" but got \"" + validation["user/email/username"].Error() + "\"")
 		}
 	})
 }
