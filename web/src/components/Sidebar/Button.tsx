7349f99fac4095571e8d56af89a9e71aab2b70de
Apostolis A <apostolis.anastasiou.alpha@gmail.com>
2020-03-12 21:05:52 +0200

    Sidebar additions & changes (#82)
    * Fix sidebar issues & page
    
    * Redirect on click

--- web/src/components/Sidebar/Button.tsx
+++ web/src/components/Sidebar/Button.tsx
@@ -1,16 +1,38 @@
 import React from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 
 interface IProps {
-  active?: boolean
+  className?: string
+  id?: string
+  to?: string
 }
 
 const Button: React.FC<IProps> = (props) => {
-  const { children, active } = props
+  const {
+    children,
+    className,
+    id,
+    to,
+  } = props
+  const location = useLocation()
+  const { pathname } = location
+  const cpPage = pathname.substr(pathname.lastIndexOf('/')+1)
+
+  const history = useHistory()
+  const handleClick = () => {
+    if (to) {
+      history.push(to)
+    }
+  }
+
   return (
     <button className={`
       sidebar__button
-      ${active ? 'sidebar__button_active' : ''}
+      ${cpPage === to ? 'sidebar__button_active' : ''}
+      ${className}
     `}
+    onClick={handleClick}
+    id={id}
     >
       {children}
     </button>
