1bfa2419e95855ed1a0484c711d3b6c7ac94c0b2
exapsy <apostolis.anastasiou.alpha@gmail.com>
2020-03-13 01:33:28 +0200

    Add onClick property on sidebar button

--- web/src/components/Sidebar/Button.tsx
+++ web/src/components/Sidebar/Button.tsx
@@ -5,6 +5,7 @@ interface IProps {
   className?: string
   id?: string
   to?: string
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const Button: React.FC<IProps> = (props) => {
@@ -13,6 +14,7 @@ const Button: React.FC<IProps> = (props) => {
     className,
     id,
     to,
+    onClick = () => null,
   } = props
   const location = useLocation()
   const { pathname } = location
@@ -20,20 +22,22 @@ const Button: React.FC<IProps> = (props) => {
   const isPathActive = cpPage === to
 
   const history = useHistory()
-  const handleClick = () => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     if (to && !isPathActive) {
       history.push(to)
     }
+    onClick(e)
   }
 
   return (
-    <button className={`
-      sidebar__button
-      ${isPathActive ? 'sidebar__button_active' : ''}
-      ${className}
-    `}
-    onClick={handleClick}
-    id={id}
+    <button
+      className={`
+        sidebar__button
+        ${isPathActive ? 'sidebar__button_active' : ''}
+        ${className}
+      `}
+      onClick={handleClick}
+      id={id}
     >
       {children}
     </button>
