9cace1b5adc900a4de7323f55ad8c5f52587ec35
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-03-15 22:12:09 +0200

    Add Date component for eventcard

---
+++ web/src/components/Calendar/Date.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+type weekday = 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat' | 'Sun'
+
+interface IDateProps {
+  day: number
+  weekday: weekday
+  month: string
+}
+
+const Date: React.FC<IDateProps> = props => {
+  const {
+    day,
+    weekday,
+    month
+  } = props
+
+  return (
+    <div className='calendar__eventcard__date'>
+      <div className="month-weekday">
+        <span className='calendar__eventcard__date__month'>
+          {month}
+        </span>
+        <span className='calendar__eventcard__date__weekday'>
+          {weekday}
+        </span>
+      </div>
+
+      <h3 className='calendar__eventcard__date__day'>
+        {day}
+      </h3>
+
+    </div>
+  )
+}
+
+export default Date
