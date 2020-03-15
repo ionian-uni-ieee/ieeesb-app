805015d7a7daa965b53782271c05d07d7baba2ff
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-03-15 22:50:48 +0200

    Add the divider component to description file

--- web/src/components/Calendar/Description.tsx
+++ web/src/components/Calendar/Description.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
+import Divider from './Divider'
 
 type type = 'Seminar' | 'Workshop'
 
+interface IProps {
+  children: ReactElement | Array<ReactElement>
+}
+
+interface IEventcard extends React.FC<IProps> {
+  Date: typeof Date
+  Description: typeof Description
+  Divider: typeof Divider
+}
 interface IPropsDescription {
   id: string
   name: string
@@ -16,7 +26,7 @@ const Description: React.FC<IPropsDescription> = props => {
     name,
     tags,
     time,
-    type
+    type,
   } = props
 
   return (
@@ -33,11 +43,11 @@ const Description: React.FC<IPropsDescription> = props => {
       <p className='calendar__eventcard__description__time'>
         {time}
       </p>
+      <Divider />
       <p className='calendar__eventcard__description__tags'>
         {tags}
       </p>
     </div>
   )
 }
-
 export default Description
