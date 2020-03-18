8990b5a69f79973f9525aa785b6e287813fd25c0
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-03-18 19:12:17 +0200

    Change children with props

--- web/src/components/Calendar/Calendar.tsx
+++ web/src/components/Calendar/Calendar.tsx
@@ -1,26 +1,25 @@
-import React, { ReactElement } from 'react'
-
-import Card from './Card'
+import React from 'react'
+import ICard from './Card'
 
 interface IProps {
-  children: ReactElement | Array<ReactElement>
+  cards: typeof ICard[]
 }
 
-interface ICalendar extends React.FC<IProps> {
-  Card: typeof Card
+const Calendar: React.FC<IProps> = props => {
+  const {
+    cards
+  } = props
+
+  return (
+    <div className='calendar'>
+      <h1 className="calendar__header">
+        Calendar
+      </h1>
+      <div className="card-item">
+        {cards.map(card => <>{card}</>)}
+      </div>
+    </div>
+  )
 }
-
-const Calendar: ICalendar = props => (
-  <div
-    className='calendar'
-  >
-    <h1 className="calendar__header">
-      Calendar
-    </h1>
-    {props.children}
-  </div>
-)
-
-Calendar.Card = Card
 
 export default Calendar
